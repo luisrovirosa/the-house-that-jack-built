22a62a1ecb68c5bf99ff194adebaecd9245921f7
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-02 09:04:56 +0100

    refactor to generate the versers on a loop

--- src/TheHouseThatJackBuilt.php
+++ src/TheHouseThatJackBuilt.php
@@ -11,8 +11,15 @@ class TheHouseThatJackBuilt
             "the malt that lay in",
             "the rat that ate"
         ];
+
+        $fullSentence = [];
+
+        for ($j = 0; $j < 1; $j++){
+            $fullSentence[0] = " " . $verses[$j];
+        }
+
         return [
-            "This is {$verses[0]}.",
+            "This is{$fullSentence[0]}.",
             "This is {$verses[1]} {$verses[0]}.",
             "This is {$verses[2]} {$verses[1]} {$verses[0]}."
         ];
