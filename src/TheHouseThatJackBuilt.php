5c80b7e8a3ed40e7ce78c6792f7df0f23b6f8946
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-02 08:36:39 +0100

    refactor reduce duplicity

--- src/TheHouseThatJackBuilt.php
+++ src/TheHouseThatJackBuilt.php
@@ -7,10 +7,11 @@ class TheHouseThatJackBuilt
     public function song(): array
     {
         $first = "the house that Jack built";
+        $second = "the malt that lay in";
         return [
             "This is $first.",
-            "This is the malt that lay in $first.",
-            "This is the rat that ate the malt that lay in $first."
+            "This is $second $first.",
+            "This is the rat that ate $second $first."
         ];
     }
 }
