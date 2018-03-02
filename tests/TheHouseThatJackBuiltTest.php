18be54ee5b1114c24def7157e7adf277726ff83e
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-02 17:07:46 +0100

    reduce duplicity

--- tests/TheHouseThatJackBuiltTest.php
+++ tests/TheHouseThatJackBuiltTest.php
@@ -8,20 +8,10 @@ use PHPUnit\Framework\TestCase;
 class TheHouseThatJackBuiltTest extends TestCase
 {
     /** @test */
-    public function the_fist_verse_is_ok()
+    public function verses_are_ok()
     {
         $this->assertVerseNumberEquals("This is the house that Jack built.", 1);
-    }
-
-    /** @test */
-    public function the_second_verse_is_ok()
-    {
         $this->assertVerseNumberEquals("This is the malt that lay in the house that Jack built.", 2);
-    }
-
-    /** @test */
-    public function the_third_verse_is_ok()
-    {
         $this->assertVerseNumberEquals("This is the rat that ate the malt that lay in the house that Jack built.", 3);
     }
 
