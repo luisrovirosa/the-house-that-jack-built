9e1a5febccf3c6efb084644c35a634fd39990423
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-12 10:58:05 +0100

    use the ReverseOrder on ReverseEchoHouse

--- src/ReverseEchoHouseThatJackBuilt.php
+++ src/ReverseEchoHouseThatJackBuilt.php
@@ -2,11 +2,14 @@
 
 namespace Codium\TheHouseThatJackBuilt;
 
+use Codium\TheHouseThatJackBuilt\Order\ReverseOrder;
+
 class ReverseEchoHouseThatJackBuilt extends EchoHouseThatJackBuilt
 {
     public function __construct()
     {
         parent::__construct();
-        $this->verses = array_reverse($this->verses);
+        $order = new ReverseOrder();
+        $this->verses = $order->sort($this->verses);
     }
 }
